30204af7c9b5b288b5adcb75f44472fa44bc0965
Shishir Pandey <Shishir.Pandey@Staples.com>
2021-01-07 20:46:08 -0500

    initial commit

---
+++ test/bucket_test.exs
@@ -0,0 +1,26 @@
+defmodule KV.BucketTest do
+    use ExUnit.Case, async: true
+
+    setup do
+        # {:ok, bucket} = KV.Bucket.start_link([])
+        bucket = start_supervised!(KV.Bucket)
+        %{bucket: bucket}
+    end
+
+
+    test "store values by key", %{bucket: bucket} do
+        assert KV.Bucket.get(bucket, "milk") == nil
+
+
+        KV.Bucket.put(bucket, "milk", 3)
+        assert KV.Bucket.get(bucket, "milk") == 3
+    end
+
+    test "get and delte values", %{bucket: bucket} do
+        KV.Bucket.put(bucket, "milk", 3)
+        KV.Bucket.put(bucket, "eggs", 2)
+
+        assert 2 == KV.Bucket.delete(bucket, "eggs")
+    end
+
+end
